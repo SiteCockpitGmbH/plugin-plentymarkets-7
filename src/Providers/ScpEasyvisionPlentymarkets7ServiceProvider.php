1e6a66c7a2a6d4c33c4528c10c86c0c9833a57a7
Hau Tran <hau.tran@nfq.com>
2025-08-11 15:29:30 +0700

    SCNFQ-45 Add custom script to template

--- src/Providers/ScpEasyvisionPlentymarkets7ServiceProvider.php
+++ src/Providers/ScpEasyvisionPlentymarkets7ServiceProvider.php
@@ -2,31 +2,12 @@
 
 namespace ScpEasyvisionPlentymarkets7\Providers;
 
-use Plenty\Plugin\ServiceProvider;
-use Plenty\Plugin\Events\Dispatcher;
-use Plenty\Modules\Template\Contracts\TemplateContainerContract;
-use Plenty\Modules\System\Modules\IO\IOEvents\IOEvents;
+use Plenty\Plugin\Templates\Twig;
 
-/**
- * Class ScpEasyvisionPlentymarkets7ServiceProvider
- * @package ScpEasyvisionPlentymarkets7\Providers
- */
-class ScpEasyvisionPlentymarkets7ServiceProvider extends ServiceProvider
+class ScpEasyvisionPlentymarkets7ServiceProvider
 {
-    /**
-    * Register the route service provider
-    */
-    public function register()
+    public function call(Twig $twig)
     {
-        $this->getApplication()->register(ScpEasyvisionPlentymarkets7RouteServiceProvider::class);
-    }
-
-    public function boot(Dispatcher $eventDispatcher)
-    {
-        $eventDispatcher->listen(IOEvents::IO_RESOURCES_IMPORT, function (TemplateContainerContract $container) {
-            // This event is fired to import resources into the theme.
-            // We use the addScriptTemplate method to inject our script.
-            $container->addScriptTemplate('ScpEasyvisionPlentymarkets7::ScpEasyvisionPlentymarkets7.twig');
-        });
+        return $twig->render('ScpEasyvisionPlentymarkets7::ScpEasyvisionPlentymarkets7');
     }
 }
