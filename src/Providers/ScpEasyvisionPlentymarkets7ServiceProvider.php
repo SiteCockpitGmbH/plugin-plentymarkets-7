76815c1853e708ce17d5136a940ac33d946694a6
Hau Tran <hau.tran@nfq.com>
2025-08-11 14:57:35 +0700

    SCNFQ-43 Add view listener

--- src/Providers/ScpEasyvisionPlentymarkets7ServiceProvider.php
+++ src/Providers/ScpEasyvisionPlentymarkets7ServiceProvider.php
@@ -3,6 +3,9 @@
 namespace ScpEasyvisionPlentymarkets7\Providers;
 
 use Plenty\Plugin\ServiceProvider;
+use Plenty\Plugin\Events\Dispatcher;
+use Plenty\Modules\Template\Contracts\TemplateContainerContract;
+use Plenty\Modules\System\Modules\IO\IOEvents\IOEvents;
 
 /**
  * Class ScpEasyvisionPlentymarkets7ServiceProvider
@@ -16,5 +19,14 @@ class ScpEasyvisionPlentymarkets7ServiceProvider extends ServiceProvider
     public function register()
     {
         $this->getApplication()->register(ScpEasyvisionPlentymarkets7RouteServiceProvider::class);
+    }
+
+    public function boot(Dispatcher $eventDispatcher)
+    {
+        $eventDispatcher->listen(IOEvents::IO_RESOURCES_IMPORT, function (TemplateContainerContract $container) {
+            // This event is fired to import resources into the theme.
+            // We use the addScriptTemplate method to inject our script.
+            $container->addScriptTemplate('ScpEasyvisionPlentymarkets7::ScpEasyvisionPlentymarkets7.twig');
+        });
     }
 }
